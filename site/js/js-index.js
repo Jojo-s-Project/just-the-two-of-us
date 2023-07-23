30bf414cd66a399aed8c5226fadbfaf0ba3ffb56
MarianaNamie <mariana.ribeiro@sptech.school>
2023-07-22 21:58:45 -0300

    html e css do index

---
+++ site/js/js-index.js
@@ -0,0 +1,447 @@
+var vt_musicas = [
+    {
+        id: 'intro01',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'intro02',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track01',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track02',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track03',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track04',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track05',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track06',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track07',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track08',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track09',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track10',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track11',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track12',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track13',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track14',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track15',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track16',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track17',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track18',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track19',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track20',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track21',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track22',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track23',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track24',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track25',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track26',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track27',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track28',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track29',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track30',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track31',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track32',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track33',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track34',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track35',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track36',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track37',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track38',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track39',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track40',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track41',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track42',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track43',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track44',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track45',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track46',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track47',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track48',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track49',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track50',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track51',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track52',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    },
+    {
+        id: 'track53',
+        nome: '',
+        artista: '',
+        duracao: '',
+        path_musica: '',
+        path_imagem: ''
+    }
+];
+
+function handleChangeTrack(event) {
+    var trackName = event.name;
+    console.log(trackName);
+}
