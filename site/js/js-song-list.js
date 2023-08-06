72cbe49c55b2ecb42b7d307112c9b785382ad064
10340314 <victor.diaz@sptech.school>
2023-08-05 21:50:48 -0300

    Adicionado player de música

---
+++ site/js/js-song-list.js
@@ -0,0 +1,92 @@
+var songList = [
+    {
+        name: "Uroko Sama no Monogatari",
+        artist: "Nagi No Asukara",
+        img: null,
+        src: "/ato1/01. Intro 1 - Nagi No Asukara - Uroko Sama no Monogatari"
+    },
+    {
+        name: "Two Sided Feeling",
+        artist: "Nagi No Asukara",
+        img: null,
+        src: "/ato1/02. Intro 2 - Nagi No Asukara - Two Sided Feeling"
+    },
+    {
+        name: "Estranhos Como Eu",
+        artist: "Tarzan",
+        img: null,
+        src: "/ato1/03. Track 01 - Tarzan - Estranhos Como Eu"
+    },
+    {
+        name: "I Won't Say I'm In Love",
+        artist: "Hercules",
+        img: null,
+        src: "/ato1/04. Track 02 - Hercules - I Won_t Say I_m In Love"
+    },
+    {
+        name: "A Whole New World",
+        artist: "Aladdin",
+        img: null,
+        src: "/ato1/05. Track 03 - Aladdin - A Whole New World"
+    },
+    {
+        name: "I See The Light",
+        artist: "Tangled",
+        img: null,
+        src: "/ato1/06. Track 04 - Tangled - I See The Light"
+    },
+    {
+        name: "All I've Ever Known",
+        artist: "Hadestown",
+        img: null,
+        src: "/ato1/07. Track 05 - Hadestown - All I_ve Ever Known"
+    },
+    {
+        name: "One Summer's Day",
+        artist: "Spirited Away",
+        img: null,
+        src: "/ato1/08. Track 06 - Spirited Away - One Summer_s Day"
+    },
+    {
+        name: "Não Olha Assim Pra Mim",
+        artist: "Outroeu",
+        img: null,
+        src: "/ato1/09. Track 07 - Outroeu - Não Olha Assim Pra Mim"
+    },
+    {
+        name: "E o Meu Peito Mais Aberto Que o Mar da Bahia",
+        artist: "Vanguart",
+        img: null,
+        src: "/ato1/10. Track 08 - Vanguart - E o Meu Peito Mais Aberto Que o Mar da Bahia"
+    },
+    {
+        name: "Bloom",
+        artist: "Troye Sivan",
+        img: null,
+        src: "/ato1/11. Track 09 - Troye Sivan - Bloom"
+    },
+    {
+        name: "Talk Dirty",
+        artist: "Doja Cat",
+        img: null,
+        src: "/ato1/12. Track 10 - Doja Cat - Talk Dirty"
+    },
+    {
+        name: "Equalize",
+        artist: "Pitty",
+        img: null,
+        src: "/ato1/13. Track 11 - Pitty - Equalize"
+    },
+    {
+        name: "Treasure",
+        artist: "Bruno Mars",
+        img: null,
+        src: "/ato1/14. Track 12 - Bruno Mars - Treasure"
+    },
+    {
+        name: "Leave the Door Open",
+        artist: "Silk Sonic",
+        img: null,
+        src: "/ato1/15. Track 13 - Silk Sonic - Leave the Door Open"
+    },
+]
---
+++ site/js/js-song.js
@@ -0,0 +1,129 @@
+const wrapper = document.querySelector(".music-player .wrapper"),
+    musicImg = wrapper.querySelector(".img-are img"),
+    musicName = wrapper.querySelector(".song-details .name"),
+    musicArtist = wrapper.querySelector(".song-details .artist"),
+    mainAudio = wrapper.querySelector("#main-audio"),
+    playPauseBtn = wrapper.querySelector(".play-pause"),
+    progressBar = wrapper.querySelector(".progress-bar"),
+    progressArea = wrapper.querySelector(".progress-area")
+
+let musicIndex = 0; // pegar da session storage
+
+// chamar função loadMusic ao carregar a página
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+})
+
+// função para carregar os dados da música
+function loadMusic(index) {
+    musicName.innerText = `${songList[index].name}`;
+    musicArtist.innerText = `${songList[index].artist}`;
+    // musicImg.src = ``;
+    mainAudio.src = `../archives/songs/${songList[index].src}.mp3`; //`images/${musics[index].img}.jpg`
+}
+
+// função para tocar a música
+function playMusic() {
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+
+// função para pausar a música
+function pauseMusic() {
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+playPauseBtn.addEventListener("click", () => {
+    const isPaused = wrapper.classList.contains("paused");
+    isPaused ? pauseMusic() : playMusic();
+})
+
+mainAudio.addEventListener("timeupdate", (e) => {
+    console.log(e);
+    
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+
+    if (currentTime == duration) {
+        wrapper.classList.remove("paused");
+        playPauseBtn.querySelector("i").innerText = "play_arrow";
+    }
+
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+    
+    
+    // mainAudio.addEventListener("loadeddata", () => {
+        // let musicCurrentTime = wrapper.querySelector(".current"),
+        // musicDuration = wrapper.querySelector(".duration");
+
+        // // atualizando tempo total da música
+        // let audioDuration = mainAudio.duration;
+        // let totalMin = Math.floor(audioDuration / 60);
+        // let totalSec = Math.floor(audioDuration % 60);
+        // if (totalSec < 10) {
+        //     totalSec = `0${totalSec}`;
+        // }
+        // musicDuration.innerText = `${totalMin}:${totalSec}`;
+
+        // // atualizando o tempo atual da música
+        // let currentMin = Math.floor(currentTime / 60);
+        // let currentSec = Math.floor(currentTime % 60);
+        // if (currentSec < 10) {
+        //     currentSec = `0${currentSec}`;
+        // }
+        // musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+    // });
+})
+
+mainAudio.addEventListener("loadeddata", () => {
+    let musicCurrentTime = wrapper.querySelector(".current"),
+        musicDuration = wrapper.querySelector(".duration");
+
+        // atualizando tempo total da música
+        let audioDuration = mainAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if (totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+})
+
+progressArea.addEventListener("click", (e) => {
+    let progressWidthVal = progressArea.clientWidth;
+    let clickedOffSetX = e.offsetX;
+    let songDuration = mainAudio.duration;
+
+    mainAudio.currentTime = (clickedOffSetX / progressWidthVal) * songDuration;
+})
+
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+// repeatBtn.addEventListener("click", () => {
+//     let getText = repeatBtn.innerText;
+//     switch(getText) {
+//         case "repeat":
+//             repeatBtn.innerText = "repeat_one";
+//             repeatBtn.setAttribute("title", "Song looped");
+//             break;
+//         case "repeat_one":
+//             repeatBtn.innerText = "repeat";
+//             repeatBtn.setAttribute("title", "");
+//             break;
+//     }
+// })
+
+mainAudio.addEventListener("ended", () => {
+    let getText = repeatBtn.innerText;
+
+    switch(getText) {
+        case "repeat_one":
+            mainAudio.currentTime = 0;
+            loadMusic(musicIndex);
+            playMusic();
+            break;
+    }
+})
