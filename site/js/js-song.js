8b2b11e6918963df56d99559bffa301096b5afe4
10340314 <victor.diaz@sptech.school>
2023-08-12 13:46:40 -0300

    adicionado lógica das letras e comentário. falta incluir todas as músicas

--- site/js/js-song.js
+++ site/js/js-song.js
@@ -5,9 +5,12 @@ const wrapper = document.querySelector(".music-player .wrapper"),
     mainAudio = wrapper.querySelector("#main-audio"),
     playPauseBtn = wrapper.querySelector(".play-pause"),
     progressBar = wrapper.querySelector(".progress-bar"),
-    progressArea = wrapper.querySelector(".progress-area")
+    progressArea = wrapper.querySelector(".progress-area"),
+    trackId = wrapper.querySelector("#track-id"),
+    divContent = document.querySelector(".part2"),
+    modalComentario = document.querySelector("#modal-comentario");
 
-let musicIndex = 0; // pegar da session storage
+let musicIndex = 2; // pegar da session storage
 
 // chamar função loadMusic ao carregar a página
 window.addEventListener("load", () => {
@@ -16,10 +19,54 @@ window.addEventListener("load", () => {
 
 // função para carregar os dados da música
 function loadMusic(index) {
+    trackId.innerText = 'Intro 01'//${sessionStorage.TRACK_ID}    
     musicName.innerText = `${songList[index].name}`;
     musicArtist.innerText = `${songList[index].artist}`;
     musicImg.src = `../archives/images/${index + 1}.png`;
-    mainAudio.src = `../archives/songs/${songList[index].src}.mp3`; //`images/${musics[index].img}.jpg`
+    mainAudio.src = `../archives/songs${songList[index].src}.mp3`; //`images/${musics[index].img}.jpg`
+
+    let hasLyrics = songList[index].hasLyrics
+    showContent(index, hasLyrics);
+}
+
+function showContent(index, hasLyrics) {
+    let content = songList[index].conteudo
+    if (!hasLyrics) {
+
+        let textContent = document.createElement("div")
+        textContent.setAttribute("class", "textContent")
+        divContent.appendChild(textContent)
+
+        content.forEach(conteudo => {
+            let paragraph = document.createElement("p");
+            paragraph.innerText = conteudo.paragrafo
+            textContent.appendChild(paragraph);
+        });
+    } else {
+        
+        let lyricsContent = document.createElement("div")
+        lyricsContent.setAttribute("class", "lyricsContent")
+        divContent.appendChild(lyricsContent)
+        
+        // praticamente igual, com adição dos comentários da Jojo nas letras
+        content.forEach(conteudo => {
+            let div = document.createElement("div");
+            
+            let hasComentario = Object.keys(conteudo).includes("comentario")
+            if (hasComentario) {
+                let comentario = criarComentario(div, conteudo.comentario)
+            }
+
+            
+            div.innerHTML = conteudo.paragrafo
+            div.classList.add("lyricsCentered")
+            lyricsContent.appendChild(div);
+        });
+    }
+}
+
+function criarComentario(div, comentario) {
+    div.classList.add("possui-comentario")
 }
 
 // função para tocar a música
